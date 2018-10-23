28405ee2f5e02f8c9d3b0b3e8407cce5cff0957b
Fred Nava <frednava67@hotmail.com>
2018-10-23 12:08:43 -0700

    push all changes to get login working

--- apps/rotation_app/views.py
+++ apps/rotation_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+import re, bcrypt
 
-# Create your views here.
+from .models import *
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX  = re.compile('[0-9]')
+
+# the index function is called when root is visited
+
+def index(request):
+    print("index()")
+
+    if "user_id" not in request.session:
+        return redirect("/login_registration")
+
+    return render(request, "home.html")
+
+    
