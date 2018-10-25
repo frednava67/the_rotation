eeb919ebd89cdca41747561c2f2801c4f8751893
Fred Nava <frednava67@hotmail.com>
2018-10-24 17:29:41 -0700

    push work for create combo

--- apps/rotation_app/urls.py
+++ apps/rotation_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^add_top$', views.show_add_top),
     url(r'^add_bottom$', views.show_add_bottom),    
     url(r'^create_combo$', views.show_combomaker),        
+    url(r'^process_combo$', views.process_combo),    
     url(r'^process_add_top$', views.process_add_top),
     url(r'^process_add_bottom$', views.process_add_bottom),
     url(r'^edit_top$', views.edit_top),
