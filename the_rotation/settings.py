3f4f69c3c90588e68fc6557a3df7475a785b107a
Ubuntu <ubuntu@ip-172-31-20-102.us-west-2.compute.internal>
2018-10-26 18:46:26 +0000

    deployment changes

--- the_rotation/settings.py
+++ the_rotation/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'osz1ky5khivq@1yo7m_vk1(7l_^0n$&bvk7q!)(-s9$=y=0eh#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.2.105', 'localhost']
+ALLOWED_HOSTS = ['54.190.198.142', '192.168.2.105', 'localhost']
 
 
 # Application definition
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
